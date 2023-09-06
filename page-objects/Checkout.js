5446df5a320e624e3ef72d007531b1339f717080
JasonelGrey <andriychuk932@gmail.com>
2023-09-06 20:46:34 +0300

    Demo

--- page-objects/Checkout.js
+++ page-objects/Checkout.js
@@ -32,7 +32,10 @@ export class Checkout {
         await this.continueToCheckoutButton.waitFor();
         await this.continueToCheckoutButton.click();
         await this.page.waitForURL(/\/login/);
-
+        console.log("hello");
+        console.log("hello");
+        console.log("hello");
+        console.log("hello");
     }
 
     
--- page-objects/LoginPage.js
+++ page-objects/LoginPage.js
@@ -11,6 +11,11 @@ export class LoginPage {
         await this.registerButton.waitFor();
         await this.registerButton.click();
         await this.page.waitForURL(/\/signup/);
-
+        console.log("hello");
+        console.log("hello");
+        console.log("hello");
+        console.log("hello");
     }
+
+
 }
