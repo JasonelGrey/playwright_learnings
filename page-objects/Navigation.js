b32f21b69f1f571eb4b8413a757294199bbf0692
JasonelGrey <andriychuk932@gmail.com>
2023-09-15 09:42:01 +0300

    Added logs

--- page-objects/Navigation.js
+++ page-objects/Navigation.js
@@ -24,6 +24,7 @@ export class Navigation {
         await this.checkoutButton.waitFor();
         await this.checkoutButton.click();
         await this.page.waitForURL("/basket");
+        console.log("Hello");
     }
 
 }
